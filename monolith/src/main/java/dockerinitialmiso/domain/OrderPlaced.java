16dd6e3a884e209904379273f771c7cadcc6954f
ChoiMiSo <127930590+Mnemosyne1234@users.noreply.github.com>
2025-06-16 12:30:53 +0900

    commit

---
+++ monolith/src/main/java/dockerinitialmiso/domain/OrderPlaced.java
@@ -0,0 +1,27 @@
+package dockerinitialmiso.domain;
+
+import dockerinitialmiso.domain.*;
+import dockerinitialmiso.infra.AbstractEvent;
+import java.time.LocalDate;
+import java.util.*;
+import lombok.*;
+
+//<<< DDD / Domain Event
+@Data
+@ToString
+public class OrderPlaced extends AbstractEvent {
+
+    private Long id;
+    private String productId;
+    private Integer qty;
+    private String customerId;
+
+    public OrderPlaced(Order aggregate) {
+        super(aggregate);
+    }
+
+    public OrderPlaced() {
+        super();
+    }
+}
+//>>> DDD / Domain Event
